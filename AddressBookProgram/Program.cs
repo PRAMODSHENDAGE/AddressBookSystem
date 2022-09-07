86d312af1a2e048b6b155cd4228156f7b6a67919
KajalShendage <shendagekajal123@gmail.com>
2022-09-08 00:41:22 +0530

    [Pramod] Add: Create Contact in Address Book

--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Address Book Program!");
+﻿using AddressBookProgram;
+Console.WriteLine("Welcome to Address Book Program");
+Contact contact = new Contact(){
+    FirstName = "Pramod",
+    LastName = "Shendage",
+    Address = "Kalamboli",
+    City = "Navi Mumbai",
+    State = "Maharastra",
+    Zip = 410218,
+    PhoneNumber = "9892033294",
+    Email = "shendagepramod@gmail.com"
+};
+Console.WriteLine("FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNumber + "\n" + "Email: " + contact.Email);
